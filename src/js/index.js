bf07e5e34fc0f5dc82e893f2d2df7a9ddb0b89cc
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2022-11-03 21:39:01 +0100

    d

---
+++ src/js/index.js
@@ -0,0 +1,136 @@
+const axios = require('axios').default;
+import { Notify } from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// URL variable
+const API = 'https://pixabay.com/api/'
+const API_KEY = '?key=30986553-a898811d242c2530c8a957c11'
+const image_type = '&image_type=photo'
+const orientation = '&orientation=horizontal'
+const safesearch = '&safesearch=true'
+const imageOnPage = 40
+let numberOfPage = 1
+//
+// DOM variable
+const inputSerch = document.querySelector('input')
+const btnSerch = document.querySelector('.btn-serch')
+const gallery = document.querySelector('.gallery')
+const loadMore = document.querySelector('#load-more')
+const form = document.querySelector('#search-form')
+//
+let bigGallery = new SimpleLightbox('.gallery a');
+
+const fetchData = async (serchWord) => {
+  let url = API + API_KEY + '&q=' + serchWord + image_type + orientation + safesearch + "&per_page=" + imageOnPage + '&page=' + numberOfPage
+  const response = await (await axios.get(url)).data
+  if (response.total === 0) {
+    Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+  } else {
+    return response
+  }
+
+}
+// fetch images
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  numberOfPage = 1
+  if (inputSerch.value.length === 0) {
+    gallery.innerHTML = ""
+    loadMore.classList.add('load-more')
+  } else {
+    gallery.innerHTML = ""
+    await fetchData(inputSerch.value).then((data) => {
+      Notify.success(`Hooray! We found ${data.totalHits} images.`)
+    })
+    await buildDomElemets()
+    await bigGallery.refresh()
+    const { height: cardHeight } = document
+      .querySelector(".gallery")
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 0.2,
+      behavior: "smooth",
+    });
+
+  }
+})
+
+// fetch more images
+
+loadMore.addEventListener('click', async() => {
+  numberOfPage += 1
+
+  await fetchData(inputSerch.value).then(async data => {
+    if (data.hits.length === 0) {
+      Notify.failure("We're sorry, but you've reached the end of search results.")
+    } else {
+      await buildDomElemets()
+      await bigGallery.refresh()
+      
+      const { height: cardHeight } = document
+        .querySelector(".gallery")
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth",
+      });
+    }
+  })
+})
+  // < img src = "${prevPhoto}" alt = "${tags}" loading = "lazy" />
+//build Dom Elements
+const buildDomElemets = async () => {
+  await fetchData(inputSerch.value).then(data => {
+    data.hits.forEach(element => {
+      const prevPhoto = element.webformatURL
+      const largePhoto = element.largeImageURL
+      const tags = element.tags
+      const likes = element.likes
+      const views = element.views
+      const comments = element.comments
+      const downloads = element.downloads
+      const photoCard =
+        `<div class="photo-card">
+  <a href="${largePhoto}"><img src="${prevPhoto}" alt="${tags}" title="${tags}"/></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <b>${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      <b>${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      <b>${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      <b>${downloads}</b>
+    </p>
+  </div>
+</div>`;
+      gallery.insertAdjacentHTML("beforeend", photoCard)
+
+    });
+    
+    
+    
+    
+  }).then(() => {
+    loadMore.classList.remove('load-more')
+    if (gallery.children.length === 0) {
+      loadMore.classList.add('load-more')
+    }
+  })
+
+}
+
+
+
+
+
