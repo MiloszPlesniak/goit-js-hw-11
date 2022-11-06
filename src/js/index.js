f3e53dfcd4b035c8a12fc9e846d6bd2d872bb9c9
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2022-11-06 18:45:43 +0100

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ const axios = require('axios').default;
 import { Notify } from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+const debounce = require('lodash.debounce');
 
 // URL variable
 const API = 'https://pixabay.com/api/'
@@ -14,7 +15,7 @@ let numberOfPage = 1
 //
 // DOM variable
 const inputSerch = document.querySelector('input')
-const btnSerch = document.querySelector('.btn-serch')
+// const btnSerch = document.querySelector('.btn-serch')
 const gallery = document.querySelector('.gallery')
 const loadMore = document.querySelector('#load-more')
 const form = document.querySelector('#search-form')
@@ -23,21 +24,59 @@ let bigGallery = new SimpleLightbox('.gallery a');
 
 const fetchData = async (serchWord) => {
   let url = API + API_KEY + '&q=' + serchWord + image_type + orientation + safesearch + "&per_page=" + imageOnPage + '&page=' + numberOfPage
-  const response = await (await axios.get(url)).data
-  if (response.total === 0) {
+  
+try {
+  const response = await (await axios.get(url))
+  if (response.data.totalHits === 0) {
+    console.log(response)
     Notify.failure("Sorry, there are no images matching your search query. Please try again.")
   } else {
-    return response
+    return response.data
+
+  }
+} catch (error) {
+  Notify.failure("We're sorry, but you've reached the end of search results.")
+  console.log(error.response.code)
+}
+}
+
+
+// fetch more images
+const infinityScroll = async () => {
+  let screanHeight = document.documentElement.clientHeight;
+  let scrollPosition = document.documentElement.scrollTop;
+  let maxScrollPosition = document.documentElement.scrollHeight;
+  if ((scrollPosition + screanHeight) > (maxScrollPosition - 5)) {
+    numberOfPage += 1
+    console.log(numberOfPage)
+    await fetchData(inputSerch.value).then(async data => {
+      if (data.hits.length === 0) {
+        Notify.failure("We're sorry, but you've reached the end of search results.")
+      } else {
+        await buildDomElemets()
+        await bigGallery.refresh()
+
+        const { height: cardHeight } = document
+          .querySelector(".gallery")
+          .firstElementChild.getBoundingClientRect();
+
+        window.scrollBy({
+          top: cardHeight * 2,
+          behavior: "smooth",
+        });
+      }
+    })
   }
 
 }
 // fetch images
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
+  window.removeEventListener('scroll', infinityScroll);
   numberOfPage = 1
   if (inputSerch.value.length === 0) {
     gallery.innerHTML = ""
-    loadMore.classList.add('load-more')
+    // loadMore.classList.add('load-more')
   } else {
     gallery.innerHTML = ""
     await fetchData(inputSerch.value).then((data) => {
@@ -53,35 +92,11 @@ form.addEventListener('submit', async (event) => {
       top: cardHeight * 0.2,
       behavior: "smooth",
     });
-
+    window.addEventListener('scroll', debounce(infinityScroll, 500));
   }
 })
 
-// fetch more images
 
-loadMore.addEventListener('click', async() => {
-  numberOfPage += 1
-
-  await fetchData(inputSerch.value).then(async data => {
-    if (data.hits.length === 0) {
-      Notify.failure("We're sorry, but you've reached the end of search results.")
-    } else {
-      await buildDomElemets()
-      await bigGallery.refresh()
-      
-      const { height: cardHeight } = document
-        .querySelector(".gallery")
-        .firstElementChild.getBoundingClientRect();
-
-      window.scrollBy({
-        top: cardHeight * 2,
-        behavior: "smooth",
-      });
-    }
-  })
-})
-  // < img src = "${prevPhoto}" alt = "${tags}" loading = "lazy" />
-//build Dom Elements
 const buildDomElemets = async () => {
   await fetchData(inputSerch.value).then(data => {
     data.hits.forEach(element => {
@@ -94,7 +109,7 @@ const buildDomElemets = async () => {
       const downloads = element.downloads
       const photoCard =
         `<div class="photo-card">
-  <a href="${largePhoto}"><img src="${prevPhoto}" alt="${tags}" title="${tags}"/></a>
+  <a href="${largePhoto}"><img src="${prevPhoto}" alt="${tags}" loading = "lazy" title="${tags}" /></a>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
@@ -117,18 +132,19 @@ const buildDomElemets = async () => {
       gallery.insertAdjacentHTML("beforeend", photoCard)
 
     });
-    
-    
-    
-    
-  }).then(() => {
-    loadMore.classList.remove('load-more')
-    if (gallery.children.length === 0) {
-      loadMore.classList.add('load-more')
-    }
   })
+  //   .then(() => {
+  //   loadMore.classList.remove('load-more')
+  //   if (gallery.children.length === 0) {
+  //     loadMore.classList.add('load-more')
+  //   }
+  // })
 
 }
+
+
+
+
 
 
 
